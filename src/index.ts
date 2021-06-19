03caa6fc9af1b69c2724e5105d273dde2f27446c
elhmn <nleme@live.fr>
2021-06-19 04:43:22 +0200

    feat: add search bar and shop view

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import path from "path";
 import exphbs from "express-handlebars";
+import * as helpers from "./helpers/handlebars";
 import { getValues, Record } from "./googlesheet";
 
 enum E_ENVIRONMENT {
@@ -11,25 +11,49 @@ enum E_ENVIRONMENT {
 
 const port: number = 3000;
 const app = express();
-const STATIC_FILES_DIRECTORY =
-  process.env.ENVIRONMENT == E_ENVIRONMENT.dev ? "../public/" : "..";
 
+//Set view path
+app.set('views', __dirname + '/views');
 app.engine(
   ".hbs",
   exphbs({
     extname: ".hbs",
     defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/",
+    helpers: helpers,
   })
 );
-
 app.set("view engine", ".hbs");
+
+
 app.get("/", async (req: any, res: any) => {
   const records: Record[] = await getValues();
 
   res.render("index", {
     records,
+    link: 'contributors',
   });
 });
+
+app.get("/shop", async (req: any, res: any) => {
+  const records: Record[] = await getValues();
+
+  res.render("index", {
+    records,
+    link: 'shop',
+  });
+});
+
+app.get("/contributors", async (req: any, res: any) => {
+  const records: Record[] = await getValues();
+
+  res.render("index", {
+    records,
+    link: 'contributors',
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`The application is listening on port ${port}`);
