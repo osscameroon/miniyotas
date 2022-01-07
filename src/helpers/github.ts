99c357f2e328e893d90cb9041a2a5a95cdfc33e1
AIR ALPHA <allphaair@gmail.com>
2022-01-08 00:42:19 +0100

    get all repos of organisation

--- src/helpers/github.ts
+++ src/helpers/github.ts
@@ -1,4 +1,10 @@
+import axios from "axios";
+
 export const extractHandleFromGitHubUrl = (url: string): string => {
   const urlArray: string[] = url?.split("/");
   return urlArray[urlArray.length - 1];
 };
+
+export const getRepos = async (url: string): Promise<any> => {
+  return await axios.get(url);
+}
---
+++ src/issues.ts
@@ -0,0 +1,36 @@
+import {getRepos} from "./helpers/github";
+
+export interface Repo {
+    title?: string,
+    description?: string,
+    language?: string,
+    issues?: number
+}
+
+const formatRepos = (items: Repo[]): Repo[] => {
+    items?.sort((a, b) => {
+        return Number(b?.issues) - Number(a?.issues);
+    });
+
+    return items;
+}
+
+export const getProjects = async (): Promise<Repo[]> => {
+    try {
+        const response = await getRepos("https://api.github.com/orgs/osscameroon/repos");
+        const repos: Repo[] = response.data.map((repository: { name: string; description: string; language: string; open_issues: string }) => {
+            return {
+                title: repository.name,
+                description: repository.description,
+                language: repository.language,
+                issues: repository.open_issues
+            };
+        })
+
+        return formatRepos(repos);
+    } catch (error) {
+        console.error("Failed to get project", error)
+    }
+
+    return [];
+}
