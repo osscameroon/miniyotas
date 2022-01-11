9d550a5bf2d31fdeb5340f04b55f3ee182e6719d
AIR ALPHA <allphaair@gmail.com>
2022-01-11 13:33:29 +0100

    Remove axios

--- src/helpers/github.ts
+++ src/helpers/github.ts
@@ -1,10 +1,4 @@
-import axios from "axios";
-
 export const extractHandleFromGitHubUrl = (url: string): string => {
   const urlArray: string[] = url?.split("/");
   return urlArray[urlArray.length - 1];
 };
-
-export const get = async (url: string): Promise<any> => {
-  return await axios.get(url);
-}
